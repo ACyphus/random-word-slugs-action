ff8c55e474964436e43bb5296cac7202b2e150ad
acyphus <983880+ACyphus@users.noreply.github.com>
2024-07-24 14:44:36 +0100

    Implement format and partsOfSpeech options

--- src/main.js
+++ src/main.js
@@ -9,12 +9,33 @@ async function run() {
   try {
     // Get inputs from the workflow file
     const numberOfWords = core.getInput('number-of-words')
+    const format = core.getInput('format')
+    const partsOfSpeech = core.getInput('parts-of-speech').split(',')
+
+    // Validate inputs
+    if (isNaN(numberOfWords) || numberOfWords <= 0) {
+      throw new Error('Invalid number of words')
+    }
+
+    // Create options object
+    const options = {
+      format,
+      partsOfSpeech
+    }
+
+    // Create kebab options object
+    const kebabOptions = {
+      format: 'kebab',
+      partsOfSpeech
+    }
 
     // Generate random words
-    const words = generateSlug(numberOfWords) // Generates a slug
+    const words = generateSlug(numberOfWords, options) // Generates a slug
+    const kebabWords = generateSlug(numberOfWords, kebabOptions) // Generates a kebab-case slug
 
     // Set outputs for other workflow steps to use
     core.setOutput('words', words)
+    core.setOutput('words-kebab-case', kebabWords)
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     // Fail the workflow run if an error occurs
