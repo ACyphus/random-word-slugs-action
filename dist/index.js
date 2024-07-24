e67d5c481d3c83bd69d270bf8817ce24c6dc40c4
acyphus <983880+ACyphus@users.noreply.github.com>
2024-07-24 15:10:47 +0100

    Add outputs for all case types

--- dist/index.js
+++ dist/index.js
@@ -3516,7 +3516,6 @@ async function run() {
   try {
     // Get inputs from the workflow file
     const numberOfWords = core.getInput('number-of-words')
-    const format = core.getInput('format')
     const partsOfSpeech = core.getInput('parts-of-speech').split(',')
 
     // Validate inputs
@@ -3526,23 +3525,50 @@ async function run() {
 
     // Create options object
     const options = {
-      format,
-      partsOfSpeech
-    }
-
-    // Create kebab options object
-    const kebabOptions = {
       format: 'kebab',
       partsOfSpeech
     }
 
     // Generate random words
-    const words = generateSlug(numberOfWords, options) // Generates a slug
-    const kebabWords = generateSlug(numberOfWords, kebabOptions) // Generates a kebab-case slug
+    const wordsString = generateSlug(numberOfWords, options) // Generates a slug string
+    const words = wordsString.split('-') // Split the slug string into an array
+
+    // Convert words to camel case
+    const camelWords = words
+      .map((word, index) => {
+        if (index === 0) {
+          return word.toLowerCase()
+        }
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+      })
+      .join('')
+
+    // Convert words to sentence case
+    const sentenceWords = words
+      .map((word, index) => {
+        if (index === 0) {
+          return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        }
+        return word.toLowerCase()
+      })
+      .join(' ')
+
+    // Convert words to lower case
+    const lowerWords = words.map(word => word.toLowerCase()).join(' ')
+
+    // Convert words to title case
+    const titleWords = words
+      .map(word => {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+      })
+      .join(' ')
 
     // Set outputs for other workflow steps to use
-    core.setOutput('words', words)
-    core.setOutput('words-kebab-case', kebabWords)
+    core.setOutput('words-kebab', wordsString)
+    core.setOutput('words-camel', camelWords)
+    core.setOutput('words-sentence', sentenceWords)
+    core.setOutput('words-lower', lowerWords)
+    core.setOutput('words-title', titleWords)
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     // Fail the workflow run if an error occurs
